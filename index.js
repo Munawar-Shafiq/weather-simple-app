1ab794ce0c4b29d5f03c98e696ee3d08ff145c8d
Muhammad Munawar Shafiq <hmunawar3360@gmail.com>
2025-07-08 21:58:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function getWeather (){
+const city = document.getElementById("city").value;
+const resultDiv = document.getElementById("result")
+if (!city) {
+    resultDiv.innerHTML = "Please enter a city name.";
+    return;
+  }
+  resultDiv.innerHTML = `Weather info for ${city} will appear here`;
+
+}
