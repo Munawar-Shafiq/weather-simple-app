cd90d6ba4165f3719326008fd3a604f0a4129419
Muhammad Munawar Shafiq <hmunawar3360@gmail.com>
2025-07-09 17:15:35 +0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function getWeather (){
+ async function getWeather (){
 const city = document.getElementById("city").value;
 const resultDiv = document.getElementById("result")
 if (!city) {
     resultDiv.innerHTML = "Please enter a city name.";
     return;
   }
-  resultDiv.innerHTML = `Weather info for ${city} will appear here`;
-
+ const weatherRes = await fetch(
+  `https://p2pclouds.up.railway.app/v1/learn/weather?city=${city}`
+ );
+ const weather =await weatherRes.json();
+ console.log(weather);
+const currentTem = weather.current.temp_c;
+resultDiv.innerHTML = `Today temperature of ${city} is ${currentTem}°C`
 }
+
+
